cdcb385e6791df691d125ed47431707e49b4d8a5
jonathan <jonathan.a.santos10@aluno.senai.br>
2026-06-18 21:29:36 -0300

    Criado arquivos pertinentes ao processo do projeto, um estilo básico de CSS e o arquivo script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 /*     <p>São insentos as pessoas menores de idade e com renda abaixo de R$ 1700,00</p>
-        <P> Até R$ 5000,00 o desonto será de 5%</P>
+      <P> Até R$ 5000,00 o desonto será de 5%</P>
         <p> De R$ 5001,00 até R$ 8000,00 o desconto será de 8%</p>
         <p> De R$ 8001,00 até R$ 10000,00 o desconto será de 10%</p>
         <P> De R$ 100001,00 será de 3%</P>
+
+        Provavelmente vou precisar imprimir isso em div result na div "div-lista-pessoas"
+        O professor quer usr modulos, arrays e repeticoes
+        Vai dar dor de cabeça mas vou tentar fazer o array ou o calculo em um modulo.
+        O resultado precisa ser tipo:
+        Nome, idade, renda digitada, desconto
 */
 
