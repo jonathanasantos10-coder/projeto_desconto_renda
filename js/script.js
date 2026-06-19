8307851295afc674c10b5b4947fc3cdd34eb25d4
jonathan <jonathan.a.santos10@aluno.senai.br>
2026-06-18 21:05:02 -0300

    Adicionado o html e parâmetros critérios

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*     <p>São insentos as pessoas menores de idade e com renda abaixo de R$ 1700,00</p>
+        <P> Até R$ 5000,00 o desonto será de 5%</P>
+        <p> De R$ 5001,00 até R$ 8000,00 o desconto será de 8%</p>
+        <p> De R$ 8001,00 até R$ 10000,00 o desconto será de 10%</p>
+        <P> De R$ 100001,00 será de 3%</P>
+*/
+
